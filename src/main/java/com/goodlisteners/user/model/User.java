59b070b513a8228e5a10f76740906c6934694521
Ana Senne <anaclarasenne@gmail.com>
2024-10-27 16:44:36 -0300

    fix id annotation

--- src/main/java/com/goodlisteners/user/model/User.java
+++ src/main/java/com/goodlisteners/user/model/User.java
@@ -1,11 +1,11 @@
 package com.goodlisteners.user.model;
 
-import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
