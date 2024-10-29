4f7ffbc16aa5b5fcbaf3656929d659aa1357d46d
arthurdepina <arthurdepinabalduinoleitao@gmail.com>
2024-10-29 00:48:11 -0300

    removing unnecessary changes

--- src/main/java/com/goodlisteners/main/App.java
+++ src/main/java/com/goodlisteners/main/App.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
     "com.goodlisteners.handlers",
     "com.goodlisteners.service",
     "com.goodlisteners.repository",
-    // "com.goodlisteners.followuser"
+    "com.goodlisteners.followuser"
 })
 public class App {
     public static void main(String[] args) {
