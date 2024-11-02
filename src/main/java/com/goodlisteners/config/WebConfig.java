f3ab8f079dec77205598c64358dd21e2a880e0c6
arthurdepina <arthurdepinabalduinoleitao@gmail.com>
2024-11-02 00:14:19 -0300

    preparing for deploy

---
+++ src/main/java/com/goodlisteners/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.goodlisteners.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Forward requests to "/" to "home.html"
+        registry.addViewController("/").setViewName("forward:/home.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
