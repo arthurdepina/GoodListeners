9d263c0932d3eeab16aabed7a321261599fd531a
arthurdepina <arthurdepinabalduinoleitao@gmail.com>
2024-11-03 09:15:21 -0300

    fixing bug. now local run is working

--- src/main/java/com/goodlisteners/config/DatabaseConfig.java
+++ src/main/java/com/goodlisteners/config/DatabaseConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
-import java.io.File;
 
 @Configuration
 public class DatabaseConfig {
@@ -15,10 +14,6 @@ public class DatabaseConfig {
     
     @Bean
     public DataSource dataSource() {
-        // Ensure the directory exists
-        File dbFile = new File(databasePath);
-        dbFile.getParentFile().mkdirs();
-        
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.sqlite.JDBC");
         dataSource.setUrl("jdbc:sqlite:" + databasePath);
