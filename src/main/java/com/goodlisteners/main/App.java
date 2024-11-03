64b14899650651bc0768c65438a6844e5b2dd14d
arthurdepina <arthurdepinabalduinoleitao@gmail.com>
2024-11-03 09:29:29 -0300

    fixing bug. now local run is working

--- src/main/java/com/goodlisteners/main/App.java
+++ src/main/java/com/goodlisteners/main/App.java
@@ -2,9 +2,9 @@ package com.goodlisteners.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+// import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+// import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
@@ -13,12 +13,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
     "com.goodlisteners.handlers",
     "com.goodlisteners.service",
     "com.goodlisteners.repository",
-    "com.goodlisteners.followuser.controller",
-    "com.goodlisteners.followuser.service",
-    "com.goodlisteners.followuser.repository"
+    // "com.goodlisteners.followuser.controller",
+    // "com.goodlisteners.followuser.service",
+    // "com.goodlisteners.followuser.repository"
 })
-@EntityScan(basePackages = "com.goodlisteners.followuser.model")
-@EnableJpaRepositories(basePackages = "com.goodlisteners.followuser.repository")
+// EntityScan(basePackages = "com.goodlisteners.followuser.model")
+// EnableJpaRepositories(basePackages = "com.goodlisteners.followuser.repository")
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
